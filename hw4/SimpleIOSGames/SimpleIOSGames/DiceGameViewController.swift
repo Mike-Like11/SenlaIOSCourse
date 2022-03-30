2cd6ff8b02a91d25d4170e785d81c8f53420a950
Mike-Like <mni007@yandex.ru>
2022-03-30 21:55:47 +0300

    added tabBar

---
+++ hw4/SimpleIOSGames/SimpleIOSGames/DiceGameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DiceGameViewController.swift
+//  SimpleIOSGames
+//
+//  Created by Mike I on 30.03.2022.
+//
+
+import UIKit
+
+class DiceGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemIndigo
+    }
+    
+
+
+}
---
+++ hw4/SimpleIOSGames/SimpleIOSGames/RockPaperScissorsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsViewController.swift
+//  SimpleIOSGames
+//
+//  Created by Mike I on 30.03.2022.
+//
+
+import UIKit
+
+class RockPaperScissorsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+
+}
